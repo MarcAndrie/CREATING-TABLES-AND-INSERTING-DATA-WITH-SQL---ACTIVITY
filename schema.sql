33d4c85c1ada7485bf046b28924cfd757c06a77f
MarcAndrie <167276784+MarcAndrie@users.noreply.github.com>
2024-09-02 00:13:26 +0800

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -47,46 +47,45 @@ CREATE TABLE GroupMembershipRequests (
 );
 
 -- Insert Records into Users Table
-INSERT INTO Users (UserID, Username, FirstName, LastName, DateOfBirth, Password)
-VALUES 
-('7365','Jethro Skegg','Jethro','Skegg','6/23/2021','gB7D{Q$9Aps2!ta'),
-('5740','Lars Stidworthy','Lars','Stidworthy','12/21/2013','nT0uAo%B{!Quyw8U3=a'),
-('6375','Riccardo Corns','Riccardo','Corns','9/28/2018','pW7$lK%tx39bWTY'),
-('2057','Suzette Stobbes','Suzette','Stobbes','11/16/2020','fW0O=yj0OIX,_a'),
-('3337','Harper Ebi','Harper','Ebi','3/20/2017','lG8y<jQV39vYs+P*T,F2g');
+INSERT INTO Users (UserID, Username, FirstName, LastName, DateOfBirth, Password) VALUES 
+(7365,'Jethro Skegg','Jethro','Skegg','1999-01-12','GhnR4'),
+(5740,'Lars Stidworthy','Lars','Stidworthy','2003-12-24','Jkh6G'),
+(6375,'Riccardo Corns','Riccardo','Corns','2021-09-15','5HkpA'),
+(2057,'Suzette Stobbes','Suzette','Stobbes','2007-03-27','NyeTQ'),
+(3337,'Harper Ebi','Harper','Ebi','2001-07-18','B8IkH');
 
 -- Insert Records into Friends Table
 INSERT INTO Friends (FriendID, FriendWhoAdded, FriendBeingAdded, IsAccepted)
 VALUES 
-(680,1,8,False),
-(764,11,9,TRUE),
-(185,1,11,TRUE),
-(438,20,4,TRUE),
-(531,3,17,FALSE);
+(1,1,1,False),
+(2,2,2,TRUE),
+(3,3,3,TRUE),
+(4,4,4,TRUE),
+(5,5,5,FALSE);
 
 -- Insert Records into Groups Table
 INSERT INTO Groups (GroupID, GroupName, CreatedBy)
 VALUES 
-(5268470922,'Emard, Strosin and Heaney',50),
-(2121705600,'Goodwin, Vandervort and Dietrich',22),
-(1631069977,'McGlynn-Bogan',33),
-(9949332826,'Goldner, Williamson and Schaefer',23),
-(0230755054,'Botsford Inc',44);
+(1,'Emard, Strosin and Heaney',1),
+(2,'Goodwin, Vandervort and Dietrich',2),
+(3,'McGlynn-Bogan',3),
+(4,'Goldner, Williamson and Schaefer',4),
+(5,'Botsford Inc',5);
 
 -- Insert Records into Posts Table
 INSERT INTO Posts (PostID, PostDescription, PostedBy, IsPublic, IsOnlyForFriends, GroupID)
 VALUES 
-(8400042964,'Fully-configurable multi-tasking architecture',44,TRUE,TRUE,6433569226),
-(7527844159,'Vision-oriented composite model',3,TRUE,FALSE,2388132959),
-(4431721711,'Automated 5th generation product',40,TRUE,FALSE,1624083943),
-(5056903868,'Mandatory stable toolset',45,TRUE,TRUE,4977373782),
-(5042317678,'Fully-configurable multi-state toolset',47,FALSE,TRUE,3788502835);
+(1,'Fully-configurable multi-tasking architecture',1,TRUE,TRUE,1),
+(2,'Vision-oriented composite model',2,TRUE,FALSE,2),
+(3,'Automated 5th generation product',3,TRUE,FALSE,3),
+(4,'Mandatory stable toolset',4,TRUE,TRUE,5),
+(5,'Fully-configurable multi-state toolset',5,FALSE,TRUE,4);
 
 -- Insert Records into Group Membership Requests Table
 INSERT INTO GroupMembershipRequests (GroupMemberShipRequestsID, GroupID, GroupMemberUserID, IsGroupMemberShipAccepted)
 VALUES 
-(3825232557,8860379555,9673680922,TRUE),
-(5392485847,5067070684,7068977154,TRUE),
-(8593985572,0373405421,6292016374,FALSE),
-(3722135621,0783598297,3173625242,TRUE),
-(3415217000,6496502692,2905717491,FALSE);
+(1,1,1,TRUE),
+(2,2,2,TRUE),
+(3,3,3,FALSE),
+(4,4,4,TRUE),
+(5,5,5,FALSE);
